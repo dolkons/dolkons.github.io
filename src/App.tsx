fe15e1ea0ab48a77bfd71eda81a87e5f515cac93
kostya dolgiy <k.dolgii@vk.team>
2026-01-12 09:34:16 +0300

    Перевёл на hashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Years from './pages/Years';
 import YearSummary from './pages/YearSummary';
